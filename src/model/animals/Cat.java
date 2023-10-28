87824938903bbf63b7fcd89e88e1fa6a3c41e6d9
artem <artempolevoj@yandex.ru>
2023-10-28 07:17:32 +0300

    update Cat

--- src/model/animals/Cat.java
+++ src/model/animals/Cat.java
@@ -1,13 +1,9 @@
 package model.animals;
 
-public class Cat extends PetsAnimal{
-    static int count = 0;
-    int id = ++count;
+public class Cat extends Pet {
 
     public Cat(String name) {
         super(name);
     }
-    public int getId() {
-        return id;
-    }
+
 }
