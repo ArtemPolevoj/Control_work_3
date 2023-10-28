51df970b4f2e18c2728da306326913e19772db89
artem <artempolevoj@yandex.ru>
2023-10-28 07:18:31 +0300

    update Donkey

--- src/model/animals/Donkey.java
+++ src/model/animals/Donkey.java
@@ -1,12 +1,8 @@
 package model.animals;
 
-public class Donkey extends PackAnimal{
-    static int count = 0;
-    int id = ++count;
+public class Donkey extends PackAnimal {
     public Donkey(String name) {
         super(name);
     }
-    public int getId() {
-        return id;
-    }
+
 }
