bd620c51b50725dd9c57eb2b227a4c57b87b8a2e
artem <artempolevoj@yandex.ru>
2023-10-28 07:17:56 +0300

    update Dog

--- src/model/animals/Dog.java
+++ src/model/animals/Dog.java
@@ -1,14 +1,8 @@
 package model.animals;
 
-public class Dog extends PetsAnimal {
-    static int count = 0;
-    int id = ++count;
+public class Dog extends Pet {
 
     public Dog(String name) {
         super(name);
     }
-    public int getId() {
-        return id;
-    }
-
 }
