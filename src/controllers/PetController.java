56ec72486ea5285667bcd723c0a58784dea3d496
artem <artempolevoj@yandex.ru>
2023-11-01 09:50:24 +0300

    update PetController

--- src/controllers/PetController.java
+++ src/controllers/PetController.java
@@ -3,77 +3,81 @@ package controllers;
 import model.FieldAnimal;
 import model.ParsDate;
 import model.Registry;
+import views.View;
 
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class PetController implements Controller {
     private final Registry registry;
-    private final Scanner scanner = new Scanner(System.in);
-
+    private View view;
     public PetController(Registry registry) {
         this.registry = registry;
-
     }
 
     @Override
     public void addNewPet() {
         HashMap<Enum<FieldAnimal>, String> data = new HashMap<>();
-        System.out.print("Введите тип питомца(dog, cat, hamster: ");
-        String type = scanner.nextLine();
+        view.print("Введите тип питомца(dog, cat, hamster): ");
+        String type = view.getString();
         if ((type.equals("dog")) || (type.equals("cat")) || (type.equals("hamster"))) {
             data.put(FieldAnimal.TYPE, type);
         } else {
-            System.out.println("Введен не верный тип питомца.");
+            view.println("Введен не верный тип питомца.");
             return;
         }
-        System.out.print("Введите имя питомца: ");
-        data.put(FieldAnimal.NAME, scanner.nextLine());
-        System.out.print("Введите дату(в формате yyyy-mm-dd): ");
-        String date = scanner.nextLine();
+        view.print("Введите имя питомца: ");
+        data.put(FieldAnimal.NAME, view.getString());
+        view.print("Введите дату(в формате yyyy-mm-dd): ");
+        String date = view.getString();
         if (ParsDate.checkDate(date)) {
             data.put(FieldAnimal.BIRTHDATE, date);
         } else {
-            System.out.println("Введена не верная дата.");
+            view.println("Введена не верная дата.");
             return;
         }
-        System.out.print("Введите новую команду(или команды через запятую: : ");
-        data.put(FieldAnimal.COMMAND, scanner.nextLine());
-        System.out.println(registry.addNewPet(data));
+        view.print("Введите новую команду(или команды через запятую: : ");
+        data.put(FieldAnimal.COMMAND, view.getString());
+        view.println(registry.addNewPet(data));
     }
 
     @Override
     public void getCommandsPet() {
-        System.out.println("Питомцы: " + registry.getAllName());
-        System.out.print("Введите имя питомца: ");
-        System.out.println(registry.getCommandsPet(scanner.nextLine()));
+        view.println("Питомцы: " + registry.getAllName());
+        view.print("Введите имя питомца: ");
+        view.println(registry.getCommandsPet(view.getString()));
     }
 
     @Override
     public void addNewCommand() {
         HashMap<Enum<FieldAnimal>, String> data = new HashMap<>();
-        System.out.println("Питомцы: " + registry.getAllName());
-        System.out.print("Введите имя питомца: ");
-        data.put(FieldAnimal.NAME, scanner.nextLine());
-        System.out.print("Введите новую команду(или команды через запятую: : ");
-        data.put(FieldAnimal.COMMAND, scanner.nextLine());
-        System.out.println(registry.addNewCommand(data));
+        view.println("Питомцы: " + registry.getAllName());
+        view.print("Введите имя питомца: ");
+        data.put(FieldAnimal.NAME, view.getString());
+        view.print("Введите новую команду(или команды через запятую: : ");
+        data.put(FieldAnimal.COMMAND, view.getString());
+        view.println(registry.addNewCommand(data));
     }
 
     @Override
     public void getPetsByBirthday() {
-        System.out.print("Введите дату(в формате yyyy-mm-dd): ");
-        System.out.println(registry.getPetsByBirthday(scanner.nextLine()));
+        view.print("Введите дату(в формате yyyy-mm-dd): ");
+        view.println(registry.getPetsByBirthday(view.getString()));
     }
 
     @Override
     public void getAll() {
-        System.out.println(registry.getAll());
+        view.println(registry.getAll());
     }
 
     @Override
     public void exit() {
-        System.out.println("Завершение работы приложения.");
+        view.println("Завершение работы приложения.");
+        view.scannerClose();
         System.exit(1);
+    }
+
+    @Override
+    public void setView(View view) {
+        this.view = view;
     }
 }
