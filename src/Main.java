115ef56c3d2efb7fea17accea8e1c13246dddc9a
artem <artempolevoj@yandex.ru>
2023-11-01 09:50:52 +0300

    update Main

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import model.RegistryPets;
 import repository.Database;
 import repository.PetsDatabase;
 import views.PetView;
+import views.View;
 
 import javax.swing.*;
 
@@ -14,6 +15,8 @@ public class Main {
         Database database = new PetsDatabase();
         Registry registry = new RegistryPets(database);
         Controller controller = new PetController(registry);
-        SwingUtilities.invokeLater(new PetView(controller));
+        View view = new PetView(controller);
+        controller.setView(view);
+        SwingUtilities.invokeLater(view);
     }
 }
